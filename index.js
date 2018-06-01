1fba4c767e76daef3817e641ce780b1b46cd7d11
Romain Le Quellec <romain.lequellec@epfedu.fr>
2018-06-01 08:17:19 +0200

    feat: add position settings

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const CONFIG_KEY = "hyperWindowSize";
-const DEFAULT_WIDTH = 500;
-const DEFAULT_HEIGHT = 500;
+const DEFAULT_WIDTH = 540;
+const DEFAULT_HEIGHT = 380;
+
+const DEFAULT_POS_X = 50;
+const DEFAULT_POS_Y = 50;
 
 let width = null;
 let height = null;
+let startX = null;
+let startY = null;
 let browserWindowSingleton = null;
 
 const trySetSize = () => {
   if (width && height && browserWindowSingleton && !browserWindowSingleton.isDestroyed()) {
     browserWindowSingleton.setSize(width, height);
   }
+  if (startX && startY && browserWindowSingleton && !browserWindowSingleton.isDestroyed()) {
+    browserWindowSingleton.setPosition(startX, startY);
+  }
 };
 
 module.exports.decorateConfig = config => {
-  width = config[CONFIG_KEY].width || DEFAULT_WIDTH;
-  height = config[CONFIG_KEY].height || DEFAULT_HEIGHT;
+
+  if(config[CONFIG_KEY].width && config[CONFIG_KEY].height) {
+    width = config[CONFIG_KEY].width || DEFAULT_WIDTH;
+    height = config[CONFIG_KEY].height || DEFAULT_HEIGHT;
+  } else {
+    console.log('hyper-window-size: \'config.hyperWindowSize\' does not contain both width and height properties');
+  }
+
+  if(config[CONFIG_KEY].startX && config[CONFIG_KEY].startY) {
+    startX = config[CONFIG_KEY].startX || DEFAULT_POS_X;
+    startY = config[CONFIG_KEY].startY || DEFAULT_POS_Y;
+  } else {
+    console.log('hyper-window-size: \'config.hyperWindowSize\' does not contain both startX and startY properties');
+  }
   trySetSize();
+
   return config;
+
 };
 
 module.exports.onWindow = browserWindow => {
